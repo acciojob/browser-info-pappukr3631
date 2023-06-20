62ad6f80d27c2b734b73324042f18940fd85ec3a
pappukr3631 <100488442+pappukr3631@users.noreply.github.com>
2023-06-20 22:20:01 +0530

    commit

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 //your JS code here. If required.
+const container = document.getElementById("browser-info");
+
+let browserInfo = navigator.userAgent;
+
+browserInfo = browserInfo.replace("/", " version ")
+
+let innerContent = `You are using ${browserInfo}`;
+
+container.innerHTML = `<p>${innerContent}</p>`;
